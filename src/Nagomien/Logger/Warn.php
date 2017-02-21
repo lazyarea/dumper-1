e848da125fe2b719b6043bd606b262577a036f02
Kazuhisa <kazuhisa@lazyarea.com>
2017-02-22 02:09:06 +0900

    Initial Commit

--- src/Nagomien/Logger/Warn.php
+++ src/Nagomien/Logger/Warn.php
@@ -4,12 +4,12 @@ namespace Nagomien\Logger;
 class Warn
 {
     private $fpath;
-    public function __construct((string)$fpath)
+    public function __construct($fpath='/tmp/dumper.log')
     {
       $this->fpath = ($fpath) ? $fpath : '/tmp/dumper.log';
     }
 
-    public function dumper($str,$data=[]){
+    public function dumper($str='',$data=[]){
         $dt = date('Y-m-d G:i:s');
         // logging timestamp
         error_log(print_r("\n[$dt] ",true),3,$this->fpath);
@@ -18,7 +18,7 @@ class Warn
             error_log("[param0 is not string] []",3,$this->fpath);
             return;
         }
-        error_log("[$str]",3,$this->fpath);
+        error_log("[$str] [",3,$this->fpath);
         // logging data part
         if ( gettype($data) == 'string' ){
             error_log("[$data]",3,$this->fpath);
@@ -29,5 +29,6 @@ class Warn
         else{
           error_log(print_r($data,true),3,$this->fpath);
         }
+        error_log("]",3,$this->fpath);
     }
 }
